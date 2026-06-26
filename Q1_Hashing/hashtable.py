134017232207896dc58395e2bbb6ff69e4666481
yinjia33 <renyinjia@gmail.com>
2026-06-27 02:05:08 +0800

    1. add update medicine, delete medicine, update part add list to choose what the user need to update 2. add category list let the user choose

--- Q1_Hashing/hashtable.py
+++ Q1_Hashing/hashtable.py
@@ -40,7 +40,7 @@ class HashTable:
         return None
 
     def display(self):
-        print("\nCurrent Medicine Inventory")
+        print("\n===================== Current Medicine Inventory =====================")
         print("-" * 70)
         print(f"{'ID':<8}{'Name':<20}{'Category':<15}{'Qty':<10}Price")
         print("-" * 70)
@@ -50,3 +50,37 @@ class HashTable:
                 print(item)
 
         print("-" * 70)
+
+    def delete(self, medicine_id):
+
+        index = self.hash_function(medicine_id)
+        start = index
+
+        while self.table[index] is not None:
+
+            if self.table[index].medicine_id == medicine_id:
+                self.table[index] = None
+                print("\nMedicine Deleted Successfully!")
+                return
+
+            index = (index + 1) % self.size
+
+            if index == start:
+                break
+
+        print("\nMedicine Not Found!")
+
+    def get_next_id(self):
+
+        used_ids = []
+
+        for medicine in self.table:
+            if medicine is not None:
+                used_ids.append(medicine.medicine_id)
+
+        new_id = 26001
+
+        while new_id in used_ids:
+            new_id += 1
+
+        return new_id
